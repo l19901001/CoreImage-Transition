9bfa4c3149d3fc2e7664f03a3e77b7c3619d142a
LEI SISI <lg_ss_long@163.com>
2017-06-18 14:29:46 +0800

    Initial Commit

---
+++ Transition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Transition
+//
+//  Created by lss on 2017/6/18.
+//  Copyright © 2017年 lss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
